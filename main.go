4b4b0039c45f639c0405cb954d458f8a960a8986
Lucas Schwalm <lucas.schwalm@roxcode.dev>
2025-08-08 09:10:20 -0300

    chore: removed useless stuff

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"log"
 	cup_router "roxscan/mug_generated/router"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load(".env")
-
-	log.Println("Starting server on port 8080...")
 	cup_router.Route("8080")
 }
